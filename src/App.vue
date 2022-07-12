0575591f52c63401d1214c277502cfd3ca0b8e76
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-12 09:38:51 -0500

    Got the scss active and changed about to cart

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link :class="{ active: $route.name === 'Home' }" to="/"
+      >Home</router-link
+    >
+    <router-link :class="{ active: $route.name === 'Cart' }" to="/cart"
+      >About</router-link
+    >
   </div>
   <router-view />
 </template>
@@ -13,5 +17,40 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
+
+body {
+  background-color: rgb(245, 245, 245);
+}
+
+#nav {
+  padding: 10px;
+  width: 100%;
+  height: 30px;
+  background-color: white;
+  line-height: 30px;
+
+  a {
+    font-weight: bold;
+    color: darkgray;
+    text-decoration: none;
+    margin: 0 5px 0 5px;
+    font-size: 1.25rem;
+
+    &.active {
+      color: #2c3e50;
+    }
+  }
+}
+
+.text-center {
+  text-align: center;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 
-const routes = [{ path: '/', component: Home }];
+const routes = [{ path: '/', name: 'Home', component: Home }];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
