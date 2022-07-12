355d6fd449732613f96d5f439f06997b1f7648eb
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-12 11:37:11 -0500

    created the items

---
+++ src/data/items.js
@@ -0,0 +1,39 @@
+const items = [
+  {
+    id: 1,
+    name: 'Comfy Slippers',
+    price: 12.0,
+    category: 'Footwear',
+    description: 'abcde',
+  },
+  {
+    id: 2,
+    name: 'Trail Runners',
+    price: 45.5,
+    category: 'Footwear',
+    description: 'fghij',
+  },
+  {
+    id: 3,
+    name: 'Hikers',
+    price: 80.0,
+    category: 'Footwear',
+    description: 'klmno',
+  },
+  {
+    id: 4,
+    name: 'Ski Jacket',
+    price: 120.0,
+    category: 'Jackets',
+    description: 'pqrst',
+  },
+  {
+    id: 5,
+    name: 'Winter Parka',
+    price: 230.5,
+    category: 'Jackets',
+    description: 'uvwxyz',
+  },
+];
+
+export default items;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import items from '../data/items.js';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+  data() {
+    return {
+      items: items,
+    };
+  },
+};
 </script>
