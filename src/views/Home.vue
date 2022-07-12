93e2424efb222a8a24ffcb9b3da0510c8d539dae
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-12 12:21:59 -0500

    Got the ProductsSummaryCard styled and everything

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="product-cards-container">
+      <ProductSummaryCard
+        v-for="product in items"
+        :key="product.id"
+        :product="product"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import items from '../data/items.js';
+import ProductSummaryCard from '../components/products/ProductSummaryCard.vue';
 
 export default {
   name: 'Home',
-  components: {},
+  components: {
+    ProductSummaryCard,
+  },
   data() {
     return {
       items: items,
@@ -18,3 +27,11 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.product-cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
