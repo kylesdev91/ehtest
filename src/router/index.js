78d131e5e658ded3dabd09a4e555eb4673d17bae
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-12 09:49:40 -0500

    "This is the cart page!"

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import Cart from '../views/Cart.vue';
 
-const routes = [{ path: '/', name: 'Home', component: Home }];
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/cart', name: 'Cart', component: Cart },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ src/views/Cart.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>This is the cart page!</div>
+</template>
