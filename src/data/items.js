6c5dd97dc2b7cf0f9bac88ab4073612aa8b0b85b
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-13 18:26:46 -0500

    Update price

--- src/data/items.js
+++ src/data/items.js
@@ -2,37 +2,37 @@ const items = [
   {
     id: 1,
     name: 'Gorila Glue 4',
-    price: 1000.5,
+    price: 1050,
     image: 'gg4.jpg',
   },
   {
     id: 2,
     name: 'Wedding Cake',
-    price: 1000.5,
+    price: 1050,
     image: 'wedding-cake.jpg',
   },
   {
     id: 3,
     name: 'Galato 33',
-    price: 1000.5,
+    price: 1050,
     image: 'gelato-33.jpg',
   },
   {
     id: 4,
     name: 'Black Runtz',
-    price: 1000.5,
+    price: 1050,
     image: 'black-runtz.jpg',
   },
   {
     id: 5,
     name: 'White Runtz',
-    price: 1000.5,
+    price: 1050,
     image: 'white-runtz.jpg',
   },
   {
     id: 6,
     name: 'Sour Diesel',
-    price: 1000.5,
+    price: 1050,
     image: 'sour-diesel.jpg',
   },
 ];
